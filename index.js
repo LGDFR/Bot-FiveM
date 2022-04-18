2675ee6bc1fba0f9250759c3ea9052ea3fadb11a
LGDFR <lucasansquer@gmail.com>
2022-04-18 13:20:48 +0200

    Code mis en place

---
+++ index.js
@@ -0,0 +1,22 @@
+const {Client, Intents,WebhookClient} = require('discord.js')
+const request = require('request')
+var argv = require('minimist')(process.argv.slice(2));
+const client = new Client({intents:[Intents.FLAGS.GUILDS]})
+const hook = new WebhookClient({url:`${argv['webhook']}`})
+client.on('ready',()=>{
+    setInterval(()=>{
+         request({url:`http://${argv['ip_port']}/dynamic.json`,timeout:60,json:true},function(error,response,body){
+    if(response === undefined){
+        client.user.setPresence({ activities: [{ name: '0 Joueurs' }], status: 'idle' });
+    }else{
+        client.user.setPresence({ activities: [{ name: `${body['clients']} Joueurs` }], status: 'online' });
+    }
+})
+
+
+    },300000)
+hook.send(`Le bot discord ${client.user.tag} est en ligne`)
+})
+
+client.login(argv['token'])
+
