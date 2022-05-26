cd74b15fae2f91cc9af21c6919525d1a0ce03565
LGDFR <lucasansquer@gmail.com>
2022-05-26 18:38:10 +0200

    License + fix
    Ajout d'une license + config.json

---
+++ README.md
@@ -0,0 +1,5 @@
+============License============
+Cette œuvre est mise à disposition selon les termes de la Licence Creative Commons Attribution - Pas d'Utilisation Commerciale - Pas de Modification 4.0 International.
+http://creativecommons.org/licenses/by-nc-nd/4.0/
+
+Support : Eway#4352
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const {Client, Intents,WebhookClient} = require('discord.js')
-const request = require('request')
-var argv = require('minimist')(process.argv.slice(2));
-const client = new Client({intents:[Intents.FLAGS.GUILDS]})
-const hook = new WebhookClient({url:`${argv['webhook']}`})
+const {Client, Intents} = require('discord.js');
+const {ip , port , token, avatar, username} = require('./config.json')
+const request = require('request');
+const client = new Client({intents:[Intents.FLAGS.GUILDS]});
 client.on('ready',()=>{
     /**Changement du nom d'utilisateur */
-    client.user.setUsername(`${argv['username']}`);
+    client.user.setUsername(`${username}`);
 
     /** Ajout d'un Avatar */
-    client.user.setAvatar(`${argv['avatarurl']}`);
+    client.user.setAvatar(`${avatar}`);
     setInterval(()=>{
-         request({url:`http://${argv['ip_port']}/dynamic.json`,timeout:60,json:true},function(error,response,body){
+         request({url:`http://${ip}:${port}/dynamic.json`,timeout:60,json:true},function(error,response,body){
     if(response === undefined){
+        
         client.user.setPresence({ activities: [{ name: '0 Joueurs' }], status: 'idle' });
     }else{
         client.user.setPresence({ activities: [{ name: `${body['clients']} Joueurs` }], status: 'online' });
@@ -20,9 +20,8 @@ client.on('ready',()=>{
 
 
     },300000)
-    console.log('Bot en ligne.')
-hook.send(`Le bot discord ${client.user.tag} est en ligne`)
+    console.log(`${client.user.tag} est en ligne`)
 })
 
-client.login(argv['token'])
+client.login(token)
 
